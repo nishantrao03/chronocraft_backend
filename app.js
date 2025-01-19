ccc2b24d005205a6640918e30228a39425ef77ea
Nishant Rao <nishantrao2003@gmail.com>
2025-01-20 02:44:45 +0530

    Origin changes 16

--- app.js
+++ app.js
@@ -55,7 +55,14 @@ app.use(cors({
   credentials: true // Allow cookies to be sent
 }));
 
-app.options('*', cors());
+// Explicitly handle preflight requests
+app.options('*', (req, res) => {
+  res.header('Access-Control-Allow-Origin', req.headers.origin); // Dynamically allow the requesting origin
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.sendStatus(200);
+});
 
 // app.use(cors({
 //   origin: '*', // Allow all origins
